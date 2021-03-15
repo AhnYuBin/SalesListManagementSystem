e50e6bc090f7d8aeca0da8885cb7e103607e9010
AhnYuBin <dbqls9373@naver.com>
2021-03-15 20:07:26 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Goods");
+		System.out.println("2. Delete Goods");
+		System.out.println("3. Edit Goods");
+		System.out.println("4. View Goods");
+		System.out.println("5. Exit");
+
+	}
+
+}
